79fb30685519f0f30f24a7e055569f66a25ae5cb
Alex Kiselev <kurbatov.lex@gmail.com>
2021-09-01 14:49:55 +0700

    fix: config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,134 +1,53 @@
 module.exports = {
 
   env: {
-
     browser: true,
-
-
-
     es2021: true,
-
-
-
-    node: true
-
+    node: trues
   },
-
-
-
+  
   extends: [
-
     'eslint:recommended',
-
-
-
     'plugin:vue/essential',
-
-
-
     'plugin:vue/vue3-recommended',
-
-
-
     'plugin:nuxt/recommended',
-
-
-
     'prettier',
-
-
-
-    'plugin:prettier/recommended'
-
+    'plugin:prettier/recommended',
   ],
-
-
 
   parser: 'vue-eslint-parser',
 
-
-
   parserOptions: {
-
     ecmaVersion: 12,
-
-
-
     sourceType: 'module'
-
   },
 
-
-
-  plugins: ['vue'],
-
-
+  plugins: ['vue', 'prettier'],
 
   rules: {
-
     'prettier/prettier': [
-
       'error',
-
-
-
       {
-
         singleQuote: true,
-
-
-
         jsxSingleQuote: true,
-
-
-
         arrowParens: 'avoid',
-
-
-
         tabWidth: 2,
-
-
-
         useTabs: false
-
       }
-
     ],
 
-
-
     'linebreak-style': [
-
-
-
       'error',
-
-
-
       process.platform === 'win32' ? 'windows' : 'unix'
-
-
-
     ]
-
   },
 
-
-
   settings: {
-
     'import/resolver': {
-
       nuxt: {
-
         extensions: ['.js', '.vue']
-
       }
-
     }
-
   }
-
 };
 
